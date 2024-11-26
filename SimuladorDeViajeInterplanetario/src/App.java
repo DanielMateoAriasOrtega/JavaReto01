b8d3ad1151b5729fcfa03880cf950d43ab0be09a
DanielMateoAriasOrtega <dmateo.arias@gmail.com>
2024-11-25 19:15:02 -0500

    Programming

--- SimuladorDeViajeInterplanetario/src/App.java
+++ SimuladorDeViajeInterplanetario/src/App.java
@@ -1,5 +1,47 @@
+import java.util.Scanner;
+
 public class App {
+    // Elementos necesarios para la ejecución
+    static Scanner scanner = new Scanner(System.in);
+    static String[] planetas = new String[]{"Mercurio", "Venus", "Tierra", "Marte", "Jupiter", "Saturno", "Urano", "Neptuno"};
+    static double[] distancias = new double[]{3.7, 8.87, 9.8, 3.7, 24.8, 10.4, 11.2, 11.1};
+    static String[] naves = new String[]{"Espacial", "EspacialMedia", "EspacialGrande", "EspacialLargaDistancia"};
+    static double[] velocidades = new double[]{1000, 2000, 3000, 4000, 5000};
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    
     }
+
+    public static void mostrarMenu() {
+        System.out.println("1. ");
+        System.out.println("2. Seleccionar Planeta");
+        System.out.println("3. Seleccionar Velocidad");
+        System.out.println("4. Calcular Tiempo");    
+    }
+
+    public static void seleccionarNave() {
+
+    }
+
+    public static void seleccionarPlaneta() {
+
+    }
+
+    public static void seleccionarVelocidad() {
+
+    }
+
+    public static void calcularTiempo() {    
+
+    }
+
+    public static void calcularDistancia() {
+
+    }
+
+    public static void calcularCombustible() {
+
+    }
+
+    
 }
